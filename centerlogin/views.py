f8c57c0d560393a6dfd99fa141f460e2b16bed52
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-05 16:26:42 +0530

    all-folder-set

--- centerlogin/views.py
+++ centerlogin/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def c(request):
+    return render(request,'c.html') 
---
+++ companylogin/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from centerlogin import views
+urlpatterns=[
+
+]
