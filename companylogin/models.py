934da525fd5a11d90fa2a1f5b45c0bda544bf3f4
Akash <9765akashanand@gmail.com>
2023-10-22 15:19:45 +0530

    ok

--- companylogin/models.py
+++ companylogin/models.py
@@ -11,6 +11,5 @@ class Centers(models.Model):
     address = models.CharField(max_length=256)
     phone = models.CharField(max_length=256)
     email = models.CharField(max_length=256)
-    profile_pic = models.ImageField(upload_to='profile_pic/center/',null=True)
+    profile_pic = models.FileField(upload_to='profile_pic/',null=True,default=None)
     password = models.CharField(max_length=128)
-    
