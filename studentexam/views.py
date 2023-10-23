8411809d3a75ceb5f4ca9a5379fcacb1f5d2ddb5
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-23 14:30:59 +0530

    exam module changes

--- studentexam/views.py
+++ studentexam/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
 # Create your views here.
 def studhome(req):
@@ -7,4 +8,11 @@ def instruction(req):
     return render(req,'instruction.html')
 
 def running(req):
-    return render(req,'running_exam.html')
+    start_time = datetime.now()
+    end_time = start_time + timedelta(hours=1)  # Set the end time to 1 hour from now
+
+    return render(req, 'running_exam.html', {
+        'start_time': start_time.strftime("%Y-%m-%d %H:%M:%S"),  # Format the time as a string
+        'end_time': end_time.strftime("%Y-%m-%d %H:%M:%S"),
+    })
+    # return render(req,'running_exam.html')
