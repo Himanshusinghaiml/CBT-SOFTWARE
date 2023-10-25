e6d7f2b9cca3ef5470f405f798bea0be486befc7
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-25 18:26:54 +0530

    running-exam-done

--- studentexam/urls.py
+++ studentexam/urls.py
@@ -4,7 +4,7 @@ urlpatterns=[
     path('student-home/',views.studhome,name="studhome"),
     path('instruction/',views.instruction,name="instruction"),
     path('running-exam/',views.running,name="running"),
-    path('over/',views.over,name="over"),
+    path('exam-over/',views.over,name="over"),
     path('get_next_question/<int:current_question_id>/', views.get_next_question, name='get_next_question'),
     path('get_previous_question/<int:current_question_id>/', views.get_previous_question, name='get_previous_question'),
 ]
